ea6dbb0dac45e21ae00992f719c11686ed96bc97
Selyaninov Stepan <soselyaninov@edu.hse.ru>
2023-03-19 22:21:46 +0300

    Implemented Node methods and iterator

--- graph.h
+++ graph.h
@@ -11,8 +11,25 @@ namespace graph {
         using weight_type = Weight;
     private:
         class Node {
+        public:
+            using const_iterator = typename std::unordered_map<key_type, weight_type>::const_iterator;
+            using iterator = typename std::unordered_map<key_type, weight_type>::iterator;
+
+            const_iterator cbegin() const noexcept { return m_edges.cbegin(); }
+            const_iterator cend() const noexcept { return m_edges.cend(); }
+            const_iterator begin() const noexcept { return m_edges.begin(); }
+            const_iterator end() const noexcept { return m_edges.end(); }
+            iterator begin() noexcept { return m_edges.begin(); }
+            iterator end() noexcept { return m_edges.end(); }
+
+            bool empty() noexcept { return m_edges.empty(); }
+            void clear() noexcept { m_edges.clear(); }
+            std::size_t size() noexcept { return m_edges.size(); }
+
+            value_type value() const noexcept { return m_value; }
+            value_type& value() noexcept { return m_value; }
         private:
-            value_type value;
+            value_type m_value;
             std::unordered_map<key_type, weight_type> m_edges;
         };
 
