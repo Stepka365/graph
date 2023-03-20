7a0320ad17631bcefc56da0623f34b89e5d193b0
Selyaninov Stepan <soselyaninov@edu.hse.ru>
2023-03-20 21:54:05 +0300

    Implemented methods for Node, methods for Graph

--- graph.h
+++ graph.h
@@ -5,13 +5,29 @@
 namespace graph {
     template<typename Key, typename Value, typename Weight>
     class Graph {
+    private:
+        class Node;
     public:
         using key_type = Key;
         using value_type = Value;
         using weight_type = Weight;
-    private:
-        class Node;
 
+        using const_iterator = typename std::unordered_map<key_type, Node>::const_iterator;
+        using iterator = typename std::unordered_map<key_type, Node>::iterator;
+
+        const_iterator cbegin() const noexcept { return m_vertices.cbegin(); }
+        const_iterator cend() const noexcept { return m_vertices.cend(); }
+        const_iterator begin() const noexcept { return m_vertices.begin(); }
+        const_iterator end() const noexcept { return m_vertices.end(); }
+        iterator begin() noexcept { return m_vertices.begin(); }
+        iterator end() noexcept { return m_vertices.end(); }
+
+
+        bool empty() const noexcept { m_vertices.empty(); }
+        std::size_t size() const noexcept { m_vertices.size(); }
+        void clear() const noexcept { m_vertices.clear(); }
+        void swap(const Graph<key_type, value_type, weight_type>& graph) { m_vertices.swap(graph.m_vertices); }
+    private:
         std::unordered_map<key_type, Node> m_vertices;
     };
 
@@ -38,4 +54,7 @@ namespace graph {
         value_type m_value;
         std::unordered_map<key_type, weight_type> m_edges;
     };
+
+    template<typename Key, typename Value, typename Weight>
+    void swap(const Graph<Key, Value, Weight>& graph1, const Graph<Key, Value, Weight>& graph2) { graph1.swap(graph2); }
 }
