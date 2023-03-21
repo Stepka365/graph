ac9a964aac8b72957b60a97506c80e272f4c15a4
Selyaninov Stepan <soselyaninov@edu.hse.ru>
2023-03-21 14:19:29 +0300

    Implemented insert_node and insert_or_assign_node

--- graph.cpp
+++ graph.cpp
@@ -2,6 +2,16 @@
 #include <algorithm>
 
 template<typename Key, typename Value, typename Weight>
+typename graph::Graph<Key, Value, Weight>::Node&
+graph::Graph<Key, Value, Weight>::at(const key_type& key) {
+    auto it = m_vertices.find(key);
+    if (it == end()) {
+        throw GraphException("Can't find key:  " + key);
+    }
+    return (*it).second;
+}
+
+template<typename Key, typename Value, typename Weight>
 std::size_t graph::Graph<Key, Value, Weight>::degree_in(const key_type& key) const {
     return std::count_if(begin(), end(), [&key](const auto& it) {
         return (*it).second.m_edges.count(key);
--- graph.h
+++ graph.h
@@ -27,15 +27,26 @@ namespace graph {
         bool empty() const noexcept { m_vertices.empty(); }
         std::size_t size() const noexcept { m_vertices.size(); }
         void clear() const noexcept { m_vertices.clear(); }
-        void swap(const Graph<key_type, value_type, weight_type>& graph) noexcept { m_vertices.swap(graph.m_vertices); }
+
+        void swap(const Graph<key_type, value_type, weight_type>& graph) noexcept {
+            m_vertices.swap(graph.m_vertices);
+        }
 
         Node& operator[](const key_type& key) { return m_vertices[key]; }
-        Node& at(const key_type& key) { return m_vertices.at(); }
+        Node& at(const key_type& key);
 
-        std::size_t degree_out(const key_type& key) const{ return m_vertices[key].size(); }
+        std::size_t degree_out(const key_type& key) const { return m_vertices[key].size(); }
         std::size_t degree_in(const key_type& key) const;
-
         bool loop(const key_type& key) const;
+
+        std::pair<iterator, bool> insert_node(const key_type& key, const value_type& value) {
+            return m_vertices.insert({key, value});
+        }
+
+        std::pair<iterator, bool> insert_or_assign_node(const key_type& key, const value_type& value) {
+            return m_vertices.insert_or_assign(key, value);
+        }
+
 
     private:
         std::unordered_map<key_type, Node> m_vertices;
@@ -66,7 +77,8 @@ namespace graph {
     };
 
     template<typename Key, typename Value, typename Weight>
-    inline void swap(const Graph<Key, Value, Weight>& graph1, const Graph<Key, Value, Weight>& graph2) noexcept {
+    inline void
+    swap(const Graph<Key, Value, Weight>& graph1, const Graph<Key, Value, Weight>& graph2) noexcept {
         graph1.swap(graph2);
     }
 
